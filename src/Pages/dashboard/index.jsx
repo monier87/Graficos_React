ddb9a5c183fab8724ae083f8faeae5184ddb2609
monier87 <carlos.monier87@gmail.com>
2023-06-19 16:23:53 -0400

    CARD DEL DASHBOARD OKK

--- src/Pages/dashboard/index.jsx
+++ src/Pages/dashboard/index.jsx
@@ -1,8 +1,86 @@
-import { Typography } from 'antd'
+import React from 'react';
+import { DollarCircleOutlined, ShopOutlined, ShoppingCartOutlined, UserOutlined } from '@ant-design/icons';
+import { Card, Typography, Space, Statistic } from 'antd';
 
 function Dashboard() {
-    return <div>
-        <Typography.Title level={5}> Dashboard</Typography.Title>
+  return (
+    <div>
+      <Typography.Title level={5}>Dashboard</Typography.Title>
+      <Space direction='horizontal'>
+        <DashboardCard
+          icon={
+            <ShoppingCartOutlined
+              style={{
+                color: "blue",
+                backgroundColor: "rgba(0,0,255,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title="Ordenes"
+          value={12345}
+        />
+        <DashboardCard
+          icon={
+            <ShopOutlined
+              style={{
+                color: "green",
+                backgroundColor: "rgba(0,255,0,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title="Inventario"
+          value={12345}
+        />
+        <DashboardCard
+          icon={
+            <UserOutlined
+              style={{
+                color: "purple",
+                backgroundColor: "rgba(0,255,255,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title="Usuarios"
+          value={12345}
+        />
+        <DashboardCard
+          icon={
+            <DollarCircleOutlined
+              style={{
+                color: "red",
+                backgroundColor: "rgba(255,0,0,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title="Renovaciones"
+          value={12345}
+        />
+      </Space>
     </div>
+  );
 }
+
+function DashboardCard({ title, value, icon }) {
+  return (
+    <Card>
+      <Space direction='horizontal'>
+        {icon}
+        <Statistic title={title} value={value} />
+      </Space>
+    </Card>
+  );
+}
+
 export default Dashboard;
