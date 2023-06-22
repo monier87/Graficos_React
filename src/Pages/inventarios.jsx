a84b6b108ecb0a19b7306ebd0bf1420f87a1432a
monier87 <carlos.monier87@gmail.com>
2023-06-22 16:06:56 -0400

    change dashboar 22-06-23

--- src/Pages/inventarios.jsx
+++ src/Pages/inventarios.jsx
@@ -1,8 +1,44 @@
-import { Typography } from 'antd'
+import { Space, Typography, Table } from 'antd';
+import { useEffect, useState } from 'react';
+import { getInventory } from '../API/API';
 
 function Inventario() {
-    return <div>
-        <Typography.Title level={5}> Inventario</Typography.Title>
-    </div>
+  const [loading, setLoading] = useState(false);
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    getInventory().then(res => {
+      setDataSource(res.products);
+    });
+  }, []);
+
+  return (
+    <Space size={20}>
+      <Typography.Title level={5}>Inventario</Typography.Title>
+      <Table
+        dataSource={dataSource}
+        columns={[
+          {
+            title: 'Title',
+            dataIndex: 'title',
+          },
+          {
+            title: 'Title',
+            dataIndex: 'title',
+          },
+          {
+            title: 'Title',
+            dataIndex: 'title',
+          },
+          {
+            title: 'Title',
+            dataIndex: 'title',
+          },
+        ]}
+      />
+    </Space>
+  );
 }
+
 export default Inventario;
